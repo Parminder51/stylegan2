4f7be2791e5468cc6e87154ac7656503c5aa7b04
Parminder Saharan <61777405+Parminder51@users.noreply.github.com>
2020-08-29 00:12:46 +0530

    print_check

--- run_generator.py
+++ run_generator.py
@@ -31,6 +31,7 @@ def generate_images(network_pkl, seeds, truncation_psi):
         print('Generating image for seed %d (%d/%d) ...' % (seed, seed_idx, len(seeds)))
         rnd = np.random.RandomState(seed)
         z = rnd.randn(1, *Gs.input_shape[1:]) # [minibatch, component]
+        print(*Gs.input_shape[1:])
         tflib.set_vars({var: rnd.randn(*var.shape.as_list()) for var in noise_vars}) # [height, width]
         images = Gs.run(z, None, **Gs_kwargs) # [minibatch, height, width, channel]
         PIL.Image.fromarray(images[0], 'RGB').save(dnnlib.make_run_dir_path('seed%04d.png' % seed))
